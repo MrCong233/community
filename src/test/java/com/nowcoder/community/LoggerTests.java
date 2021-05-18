9c3ac0f614464ec0459da3c4d06c1a5d722c1551
Cong Zhou <943464315@qq.com>
2021-05-18 21:00:16 +0800

    test for commit

--- src/test/java/com/nowcoder/community/LoggerTests.java
+++ src/test/java/com/nowcoder/community/LoggerTests.java
@@ -10,6 +10,8 @@ import org.springframework.test.context.ContextConfiguration;
 @ContextConfiguration(classes = CommunityApplication.class)
 public class LoggerTests {
 
+    //git测试一下啦
+    private int test = 1;
     //实例化Loger接口
     //输入为一个类，类名就是Logger的名字，用于区别不同的logger，一般使用主类
     private static final Logger logger = LoggerFactory.getLogger(LoggerTests.class);
